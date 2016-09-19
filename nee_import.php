b3e28aed023f268236525a0d7f38480281839eba
Your Name <you@example.com>
2016-09-19 20:38:57 +0500

    First time

---
+++ nee_import.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package Nee Import
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Nee Import
+Plugin URI: 
+Description: post imports
+Author:  Naeem Shah
+Version: 1.0.0
+Author URI: 
+*/
+
+if ( !function_exists( 'add_action' ) ) 
+	die('Hi there!  I\'m just a plugin, not much I can do when called directly.');
